fd6ebc7ce398704d467dd3bba8c4a97a7468981c
Sam L. Thomas <st@simulacra.to>
2020-06-18 22:41:59 +0200

    add some missing features per bug #42692

--- include/llvm-c/Extras/Core.h
+++ include/llvm-c/Extras/Core.h
@@ -6,7 +6,27 @@
 
 LLVM_C_EXTERN_C_BEGIN
 
+typedef enum {
+  LLVMFastMathFlagsNone = 0,
+  LLVMFastMathFlagsAllowReassoc = (1 << 0),
+  LLVMFastMathFlagsNoNaNs = (1 << 1),
+  LLVMFastMathFlagsNoInfs = (1 << 2),
+  LLVMFastMathFlagsNoSignedZeros = (1 << 3),
+  LLVMFastMathFlagsAllowReciprocal = (1 << 4),
+  LLVMFastMathFlagsAllowContract = (1 << 5),
+  LLVMFastMathFlagsApproxFunc = (1 << 6),
+} LLVMFastMathFlags;
+
 LLVMValueRef LLVMGetFirstInsertionPt(LLVMBasicBlockRef BB);
+
+LLVMBool LLVMHasNoInfs(LLVMValueRef Instr);
+LLVMBool LLVMHasNoNaNs(LLVMValueRef Instr);
+LLVMBool LLVMHasNoSignedWrap(LLVMValueRef Instr);
+LLVMBool LLVMHasNoSignedZeros(LLVMValueRef Instr);
+LLVMBool LLVMHasNoUnsignedWrap(LLVMValueRef Instr);
+LLVMBool LLVMIsExact(LLVMValueRef Instr);
+LLVMBool LLVMIsFast(LLVMValueRef Instr);
+LLVMFastMathFlags LLVMGetFastMathFlags(LLVMValueRef Instr);
 
 LLVM_C_EXTERN_C_END
 
--- src/llvm-core-extras.cc
+++ src/llvm-core-extras.cc
@@ -1,5 +1,9 @@
 #include <llvm-c/Core.h>
+#include <llvm-c/Extras/Core.h>
 #include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instruction.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/Operator.h>
 
 using namespace llvm;
 
@@ -9,4 +13,52 @@ LLVMValueRef LLVMGetFirstInsertionPt(LLVMBasicBlockRef BB) {
   if (I == Block->end())
     return nullptr;
   return wrap(&*I);
+}
+
+LLVMBool LLVMHasNoInfs(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->hasNoInfs();
+}
+
+LLVMBool LLVMHasNoNaNs(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->hasNoNaNs();
+}
+
+LLVMBool LLVMHasNoSignedWrap(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->hasNoSignedWrap();
+}
+
+LLVMBool LLVMHasNoSignedZeros(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->hasNoSignedZeros();
+}
+
+LLVMBool LLVMHasNoUnsignedWrap(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->hasNoUnsignedWrap();
+}
+
+LLVMBool LLVMIsExact(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->isExact();
+}
+
+LLVMBool LLVMIsFast(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  return I->isFast();
+}
+
+LLVMFastMathFlags LLVMGetFastMathFlags(LLVMValueRef Instr) {
+  Instruction *I = unwrap<Instruction>(Instr);
+  FastMathFlags FMF = I->getFastMathFlags();
+  return static_cast<LLVMFastMathFlags>(
+    (FMF.allowContract() ? LLVMFastMathFlagsAllowReassoc : LLVMFastMathFlagsNone) |
+    (FMF.noNaNs() ? LLVMFastMathFlagsNoNaNs : LLVMFastMathFlagsNone) |
+    (FMF.noInfs() ? LLVMFastMathFlagsNoInfs : LLVMFastMathFlagsNone) |
+    (FMF.noSignedZeros() ? LLVMFastMathFlagsNoSignedZeros : LLVMFastMathFlagsNone) |
+    (FMF.allowReciprocal() ? LLVMFastMathFlagsAllowReciprocal : LLVMFastMathFlagsNone) |
+    (FMF.allowContract() ? LLVMFastMathFlagsAllowContract : LLVMFastMathFlagsNone) |
+    (FMF.approxFunc() ? LLVMFastMathFlagsApproxFunc : LLVMFastMathFlagsNone));
 }
